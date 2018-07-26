81055430010ff7671c6248110ddd212b8452e232
Emily Welsch <emwelsch@gmail.com>
2018-07-26 18:53:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,19 +25,6 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  # def save
-  #   if self.id
-  #     self.update
-  #   else
-  #     sql = <<-SQL
-  #       INSERT INTO students (name, grade)
-  #       VALUES (?, ?)
-  #     SQL
-  #     DB[:conn].execute(sql, self.name, self.grade)
-  #     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-  #   end
-  # end
-
   def save
     if self.id
       self.update
@@ -46,9 +33,7 @@ class Student
         INSERT INTO students (name, grade)
         VALUES (?, ?)
       SQL
-
       DB[:conn].execute(sql, self.name, self.grade)
-
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
   end
