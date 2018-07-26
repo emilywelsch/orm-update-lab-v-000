ceafdcb61d16aec3209b9de31e0c3cfaac147fad
Emily Welsch <emwelsch@gmail.com>
2018-07-26 17:41:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,13 +1,34 @@
 require_relative "../config/environment.rb"
 
 class Student
-attr_accessor :name, :grade
-attr_reader :id
+  attr_accessor :name, :grade
+  attr_reader :id
 
-def initialize(name, grade, id = nil)
-  @name = name
-  @grade = grade
-  @id = id
-end
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def self.create_table
+  end
+
+  def self.drop_table
+  end
+
+  def save
+  end
+
+  def self.create
+  end
+
+  def self.new_from_db
+  end
+
+  def find_by_name
+  end
+
+  def update
+  end
 
 end
